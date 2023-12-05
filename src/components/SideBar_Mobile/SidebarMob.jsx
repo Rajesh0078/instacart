e8cea82ff14f14869940bb2db67c3e00a29e95a3
rajesh <peketirajesh007@gmail.com>
2023-12-06 00:05:49 +0530

    scroll removed

--- src/components/SideBar_Mobile/SidebarMob.jsx
+++ src/components/SideBar_Mobile/SidebarMob.jsx
@@ -10,8 +10,14 @@ const SidebarMob = () => {
 
     const { isSideBarOpen, setIsSideBarOpen, setToken, setUser, user } = useContext(store)
 
+    if (isSideBarOpen) {
+        document.body.style.height = "100vh"
+    } else {
+        document.body.style.height = "100%"
+    }
+
     return (
-        <div className={`md:hidden z bg-white w-full h-full sidebar ${isSideBarOpen ? "activex" : ""}`}>
+        <div className={`md:hidden z bg-white w-full min-h-screen sidebar ${isSideBarOpen ? "activex" : ""}`}>
             <div className='flex items-center border-b p-3'>
                 <Link to={'/'}><img src="/assets/logo.png" alt="logo" className='w-[8rem] slide-right inline' /></Link>
                 <MdCancel className='text-green-900 text-3xl inline ms-auto ' role='button' onClick={() => setIsSideBarOpen(false)} />
