519e9b2bcdc70befd45fd0e21e0141780b0d7f3f
rajesh <peketirajesh007@gmail.com>
2023-12-22 15:11:17 +0530

    ra

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,18 +8,31 @@ const Home = () => {
     return (
         <section className='px-[1.5rem] lg:px-[5rem] lg:py-0 py-3'>
             <div className=' flex justify-between gap-3 flex-col'>
-                <div className='w-full  md:h-[20rem] h-[8rem] rounded-2xl md:m-0 mt-[4rem] border green_radient'>
+                <div className='w-full  md:h-[20rem] h-[8.5rem] rounded-2xl md:m-0 mt-[4rem] border green_radient'>
                     {/* <img src="/assets/raj.png" alt="" className=' res_img object-cover' /> */}
                 </div>
-                <div className=' w-full lg:h-[58%] md:h-[50%] flex gap-7 mb-3'>
-                    <div className='basis-1/3 lg:h-[25rem]'>
-                        <img src="/assets/1.webp" alt="offerimg" className='  rounded-2xl res_img' />
+                <div className=' w-full lg:h-[20%]  flex gap-3 mb-3'>
+
+                    <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded lg:block">
+                        <img
+                            src='/assets/1.webp'
+                            alt={"ka"}
+                            className="h-full w-full object-cover object-center"
+                        />
                     </div>
-                    <div className='basis-1/3 lg:h-[25rem] border image'>
-                        {/* <img src="/assets/2.webp" alt="offerimg" className=' rounded-2xl res_img' /> */}
+                    <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded lg:block">
+                        <img
+                            src='/assets/2.webp'
+                            alt={"ka"}
+                            className="h-full w-full object-cover object-center"
+                        />
                     </div>
-                    <div className='basis-1/3 lg:h-[25rem] image3'>
-                        {/* <img src="/assets/03.webp" alt="offerimg" className='rounded-2xl  res_img' /> */}
+                    <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded lg:block">
+                        <img
+                            src='/assets/03.webp'
+                            alt={"ka"}
+                            className="h-full w-full object-cover object-center"
+                        />
                     </div>
                 </div>
             </div>
